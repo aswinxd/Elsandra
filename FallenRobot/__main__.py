c3aaa9544a57b897d78a2e5d408ef97e802e6c7a
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-13 16:44:56 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -198,14 +198,13 @@ def start(update: Update, context: CallbackContext):
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
             )
-     else:
+    else:
         update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+          "Iam online  !\n<b>UPDATE​:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
         )
-
 
 def error_handler(update, context):
     """Log the error and send a telegram message to notify the developer."""
