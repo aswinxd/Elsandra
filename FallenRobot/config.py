caa89589ad454fdd26968faed7374580517629b8
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-13 16:37:58 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -21,11 +21,11 @@ class Config(object):
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = [6756452709]  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    DRAGONS = [5619966662]  # User id of sudo users
+    DEV_USERS = [5619966662]  # User id of dev users
+    DEMONS = [5619966662]  # User id of support users
+    TIGERS = [5619966662]  # User id of tiger users
+    WOLVES = [5619966662]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
